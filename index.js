84b0715824453b47303e7ce9c53d2f1f0b794ef6
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-11 20:04:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // add solution here
+var empty = []
+
+function theBeatlesPlay(musicians, instruments) {
+  for (let i = 0; i < musicians.length; i++) {
+    empty.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return empty
+}
+
