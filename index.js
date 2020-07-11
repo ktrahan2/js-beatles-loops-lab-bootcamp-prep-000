23ba57743325277c48676240a5d357c2198ed447
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-11 20:14:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function theBeatlesPlay(musicians, instruments) {
 
 function johnLennonFacts(array) {
   let i = 0
-  var newarray = array[`${i}!!!`]
+  var newarray = [`${i}!!!`]
   while (i < array.length) {
   return newarray
   }
